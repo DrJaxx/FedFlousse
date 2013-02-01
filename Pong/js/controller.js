1fb2cc9593e5423147e43a92cb1e64112c405fae
DrJaxx <durufe@gmail.com>
2013-02-02 00:31:19 +0100

    Added controller

---
+++ Pong/js/controller.js
@@ -0,0 +1,54 @@
+//Ctrl is an object which's used to get player's actions.
+
+var ctrl = {};
+
+//Keys used to navigate
+
+ctrl.upvalue = 37;  // 37 = left arrow of the keyboard
+ctrl.downvalue = 39; // 39 = right arrow of the keyboard
+
+//Initialize both to false at the start of the game. The player won't move if both are false.
+
+ctrl.up = false;
+ctrl.down = false;
+
+// Set value to true when the keys are pressed
+
+$(document).bind('keydown',function(){
+		if(e.keyCode == ctrl.upvalue){
+			ctrl.up = true;
+			return false;
+		} else if(e.keyCode == ctrl.downvalue){
+			ctrl.down = true;
+			return false;
+		}
+});
+
+// Set value to false when the keys are released
+
+$(document).bind('keyup',function(){
+		if(e.keyCode == ctrl.upvalue){
+			ctrl.up = false;
+			return false;
+		} else if(e.keyCode == ctrl.downvalue){
+			ctrl.down = false;
+			return false;
+		}
+});
+
+// 3 states possible for the controller : none, up, down.
+
+ctrl.state = function(){
+	if (ctrl.up == false && ctrl.down == false || ctrl.up == true && ctrl.down == true){
+		return "none";
+	}
+	else if (ctrl.up = true){
+		return "up";
+	}
+	else{
+		return "down";
+	}
+};
+
+
+
