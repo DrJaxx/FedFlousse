8cefff2ca64b20d284f8b812c382d7c3d6982098
Florentin Monteil <florentin.monteil@gmail.com>
2013-02-01 23:33:45 +0100

    Second commit

--- Pong/js/main.js
+++ Pong/js/main.js
@@ -1,7 +1,21 @@
 $(document).ready(function(){
+
 	var canvas = document.getElementById('pong');
 	var ctx = canvas.getContext('2d');
 	ctx.fillStyle = "white";
-	ctx.fillRect(0,25,100,10);
+	ctx.fillRect(0,25,10,100);
+
+	var raquette = {};
+	raquette.width = 10;
+	raquette.height = 100;
+
+	var player = {};
+	player.position = 25;
+
+	var haut = 38;
+	var bas = 40;
+	$(document).bind('keydown',function(e){
+		console.log(e.keyCode);
+	});
 
 });
