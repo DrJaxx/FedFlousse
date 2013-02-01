beb905a7468e9e69c788fc2fb18157f432cba21e
DrJaxx <durufe@gmail.com>
2013-02-01 23:35:36 +0100

    Commentaires sur le début

--- Pong/js/main.js
+++ Pong/js/main.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
-	var canvas = document.getElementById('pong');
-	var ctx = canvas.getContext('2d');
+	var canvas = document.getElementById('pong'); //Choper la zone de l'écran
+	var ctx = canvas.getContext('2d'); //Pour définir l'élément canvas 
+	
 	ctx.fillStyle = "white";
 	ctx.fillRect(0,25,100,10);
 
